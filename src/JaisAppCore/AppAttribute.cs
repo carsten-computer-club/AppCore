d8bd73efe21387e1c4c28cbaf4bf5b5f76e30e69
Julian Baumann <git@julba.de>
2022-04-02 22:55:41 +0200

    Initial commit

---
+++ src/JaisAppCore/AppAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using Avalonia;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+
+namespace JaisAppCore;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+public class AppAttribute : Attribute
+{
+    public string AppName { get; }
+    public Bitmap AppIcon { get; }
+
+    public AppAttribute(string name, string iconPath)
+    {
+        AppName = name;
+
+        string? assemblyName = Assembly.GetEntryAssembly()?.GetName()?.Name;
+        var assets = AvaloniaLocator.Current.GetService<IAssetLoader>();
+        Stream? asset = assets?.Open(new Uri($"avares://{assemblyName}{iconPath}"));
+
+        AppIcon = new Bitmap(asset);
+    }
+}
